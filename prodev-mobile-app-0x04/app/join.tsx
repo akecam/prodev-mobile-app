eac129e15744cb98097fc5bb1e9830dda7ad3b63
Donald Umeh <donaldumeh54@gmail.com>
2025-09-03 15:39:26 +0100

    navigation using stack navigation patterns

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -15,7 +15,7 @@ export default function Index() {
       <SafeAreaView style={styles.container}>
         <View style={styles.navGroup}>
           <Ionicons name="arrow-back" size={25} />
-          <Image source={require("@/assets/images/Logo.png")} />
+          <Image source={require("@/assets/images/logo.png")} />
         </View>
         <Text style={styles.largeText}>Create Your</Text>
         <Text style={styles.largeText}>Account</Text>
